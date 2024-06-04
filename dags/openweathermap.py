630747be2ff0875f7a84e8962607db7407a6ca34
Julián Pachón Castrillón <jupachon@unal.edu.co>
2024-06-04 15:12:35 -0500

    feature: Definition of the ETL DAG

---
+++ dags/openweathermap.py
@@ -0,0 +1,38 @@
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+from utils import env_variables
+from datetime import datetime, timedelta, date
+from scripts import extract, transform, load
+
+default_args = {
+    "owner": "Julián Pachón Castrillón",
+    "depends_on_past": False,
+    "start_date": datetime(2024, 6, 4),
+    'email': ['julianpachon506@gmail.com'],
+    "email_on_failure": False,
+    "email_on_retry": False,
+    "retries": 1,
+    "retry_delay": timedelta(seconds=60),
+}
+
+with DAG(
+    "openweathermap_dag",
+    default_args=default_args,
+    schedule_interval=timedelta(minutes=5),
+    catchup=False,
+) as dag:
+    extract_current_weather_data = PythonOperator(
+        task_id="extract_current_weather_data",
+        python_callable=extract.extractCurrentWeatherData,
+    )
+
+    transform_extracted_data = PythonOperator(
+        task_id="transfrom_extracted_data",
+        python_callable=transform.transformExtractedData,
+    )
+
+    load_extracted_data = PythonOperator(
+        task_id="load_extracted_data", python_callable=load.upload_data
+    )
+
+    extract_current_weather_data >> transform_extracted_data >> load_extracted_data
